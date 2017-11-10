44cdd767e863eae52837586d3cd1c17d3e9ae2bf
Seung ho Jung <jungseungho@gmail.com>
2017-11-10 14:15:26 -0500

    v2.1
    added instruction texts, & "z" cheat key.

--- Mike & His Beautiful Wedding/Assets/Scripts/move.cs
+++ Mike & His Beautiful Wedding/Assets/Scripts/move.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class move : MonoBehaviour {
 
 	public float speed = 1.0f;
+	public float speed1 = 1.0f;
 
 	void Update () {
 		if (Input.GetKeyDown("space") == true){
 			transform.Translate (0, 0, speed);
+		}
+		if (Input.GetKey("z") == true){
+			transform.Translate (0, 0, speed1);
 		}
 	}
 }
